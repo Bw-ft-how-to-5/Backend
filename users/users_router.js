2f7c6bd1d9b7ecae9f450259fdcd3c00a9446736
Robert Elias <RobertElias@users.noreply.github.com>
2020-03-03 14:03:51 -0800

    updates

--- users/users_router.js
+++ users/users_router.js
@@ -25,22 +25,43 @@ router.post("/register", (req, res) => {
       });
   });
   
-  router.post("/login", (req, res) => {
-    const { username, password } = req.body;
-    Users.getBy( username )
+//   router.post("/login", (req, res) => {
+//     const { username, password } = req.body;
+//     Users.getBy( username )
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           const token = generateToken(user);
+//           res
+//             .status(200)
+//             .json({ message: `Welcome back, ${username}`, user, token: token });
+//         } else {
+//           res.status(401).json({ message: "invalid username/password" });
+//         }
+//       })
+//       .catch(err => {
+//         res.status(500).json({ Error: "failed to retrieve database", err });
+//       });
+//   });
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+  
+    Users.findBy({ username })
       .first()
       .then(user => {
         if (user && bcrypt.compareSync(password, user.password)) {
-          const token = generateToken(user);
-          res
-            .status(200)
-            .json({ message: `Welcome back, ${username}`, user, token: token });
+          const token = generateToken(user); // get token
+          res.status(200).json({
+            message: `Welcome ${user.username}!`,
+            token, // send token
+          });
         } else {
-          res.status(401).json({ message: "invalid username/password" });
+          res.status(401).json({ message: 'Invalid Credentials' });
         }
       })
-      .catch(err => {
-        res.status(500).json({ Error: "failed to retrieve database", err });
+      .catch(error => {
+        console.log(error);
+        res.status(500).json(error);
       });
   });
   
@@ -123,16 +144,16 @@ router.delete('/:id', auth, (req, res) => {
       })
     
 });
-function generateToken(user){
-    const payload = {
-      username: user.username,
-    };
-    const secret = process.env.JWT_SECRET || "is it secret, or is it safe";
-    const options = {
-      expiresIn: '1h'
-    };
-    return jwt.sign(payload, jwtSecret, options)
-  }
+// function generateToken(user){
+//     const payload = {
+//       username: user.username,
+//     };
+//     const secret = process.env.JWT_SECRET || "is it secret, or is it safe";
+//     const options = {
+//       expiresIn: '1h'
+//     };
+//     return jwt.sign(payload, jwtSecret, options)
+//   }
 
   function generateToken(user) {
     const payload = {
@@ -142,7 +163,7 @@ function generateToken(user){
     const options = {
       expiresIn: "8h"
     };
-    const token = jwt.sign(payload, secret.jwtSecret, options);
+    const token = jwt.sign(payload, jwtSecret, options);
     return token;
   }
 module.exports = router;
